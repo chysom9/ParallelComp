1f525715da47f553fa64b57968329d9bf551a5cb
chi <cnwosu2@charlotte.edu>
2025-02-02 18:17:35 -0500

    recoded this becuase it emssed up lol

--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
-using namespace std;
+#include <chrono>
+#include <cstdlib>
 
-void merge(int leftArr[], int rightArr[], int arr[], int leftLen, int rightLen) {
+using namespace std;
+using namespace std::chrono;
+void merge(int leftArr[], int rightArr[], int arr[], int leftLen, int rightLen)
+{
     int i = 0, l = 0, r = 0;
 
     // Merge elements from leftArr and rightArr
-    while (l < leftLen && r < rightLen) {
-        if (leftArr[l] < rightArr[r]) {
+    while (l < leftLen && r < rightLen)
+    {
+        if (leftArr[l] < rightArr[r])
+        {
             arr[i++] = leftArr[l++];
-        } else {
+        }
+        else
+        {
             arr[i++] = rightArr[r++];
         }
     }
 
     // Add remaining elements from leftArr
-    while (l < leftLen) {
+    while (l < leftLen)
+    {
         arr[i++] = leftArr[l++];
     }
 
-    // Add remaining elements from rightArr
-    while (r < rightLen) {
+    // Add remaining elements from rightArr/
+    while (r < rightLen)
+    {
         arr[i++] = rightArr[r++];
     }
 }
 
-void mergeSort(int arr[], int len) {
-    if (len <= 1) {
+void mergeSort(int arr[], int len)
+{
+    if (len <= 1)
+    {
         return;
     }
 
@@ -36,10 +48,12 @@ void mergeSort(int arr[], int len) {
     int *rightArr = new int[len - middle];
 
     // Fill the left and right subarrays
-    for (int i = 0; i < middle; i++) {
+    for (int i = 0; i < middle; i++)
+    {
         leftArr[i] = arr[i];
     }
-    for (int i = middle; i < len; i++) {
+    for (int i = middle; i < len; i++)
+    {
         rightArr[i - middle] = arr[i];
     }
 
@@ -55,16 +69,34 @@ void mergeSort(int arr[], int len) {
     delete[] rightArr;
 }
 
-int main() {
-    int arr[] = {1,30,33,51,66,6, 3, 2, 4, 5, 6,100, 7, 8, 9, 10};
-    int len = sizeof(arr) / sizeof(arr[0]);
+int main()
+{
+    cout << "Enter teh amount of numbers you want in the array";
+    int size;
+    cin >> size;
+    int *arr = new int[size];
+    for(int i= 0; i < size; i ++) arr[i] = rand() % 100;
+    cout << "Unsorted Array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
-    mergeSort(arr, len);
+    cout << endl;\
+    auto start = high_resolution_clock::now();
+    mergeSort(arr, size);
+    auto stop = high_resolution_clock::now();
 
-    for (int i = 0; i < len; i++) {
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Sorted Array: ";
+    for (int i = 0; i < size; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
+
+    cout << "Sort Time: " << duration.count() << " ms" << endl;
+    cerr << "Sort Time: " << duration.count() << " ms" << endl;
 
     return 0;
 }
